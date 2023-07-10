6224d7e1b67c31511c783d80203192e80136d26c
TeoRiga96 <rigamonti.teo@gmail.com>
2023-07-10 07:31:29 +0200

    cartella starterKit

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
-const TODO: string = "start writing your Express API server here :)";
+import express from 'express';
+import morgan from 'morgan';
 
+const TODO: string = "start writing your Express API server here :)";
 console.log(TODO);
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.json({msg : 'Hello World -> questa risposta va al browser!'})
+})
+
+app.listen(3000, function(){
+    console.log("il server è in ascolto alla porta 3000")
+})
